0e00e9b83c77ccb9a14b2b61ee86973450ddc377
Anton Tabachenko <antontab91@gmail.com>
2025-11-23 14:32:28 +0200

    feat: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 8080
-  },
-  preview: {
-    port: 4173
-  }
+    base: '/stepper-test-task/',
+
+    server: {
+        port: 8080,
+    },
+
+    preview: {
+        port: 4173,
+    },
 });
